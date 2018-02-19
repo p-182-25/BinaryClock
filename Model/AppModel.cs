881d0846b0d91838329149c194b3620453f03c21
plisewski <32065830+plisewski@users.noreply.github.com>
2018-02-19 20:34:00 +0100

    Add files via upload

---
+++ Model/AppModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryClockTest.Model
+{
+    public class AppModel
+    {
+        // int h10 = DateTime.Now.Hour / 10;
+        // int h1 = DateTime.Now.Hour % 10;
+        // int min10 = DateTime.Now.Minute / 10;
+        // int min1 = DateTime.Now.Minute % 10;
+        // int sec10 = DateTime.Now.Second / 10;
+        // int sec1 = DateTime.Now.Second % 10;
+
+        public int hour10 { get; set; }
+        public int hour1 { get; set; }
+        public int min10 { get; set; }
+        public int min1 { get; set; }
+        public int sec10 { get; set; }
+        public int sec1 { get; set; }
+
+    }
+}
