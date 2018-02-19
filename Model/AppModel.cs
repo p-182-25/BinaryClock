06194cc8f88057d6db2902ad6519abb73d75811f
plisewski <32065830+plisewski@users.noreply.github.com>
2018-02-19 20:35:48 +0100

    Update AppModel.cs

--- Model/AppModel.cs
+++ Model/AppModel.cs
@@ -8,19 +8,11 @@ namespace BinaryClockTest.Model
 {
     public class AppModel
     {
-        // int h10 = DateTime.Now.Hour / 10;
-        // int h1 = DateTime.Now.Hour % 10;
-        // int min10 = DateTime.Now.Minute / 10;
-        // int min1 = DateTime.Now.Minute % 10;
-        // int sec10 = DateTime.Now.Second / 10;
-        // int sec1 = DateTime.Now.Second % 10;
-
         public int hour10 { get; set; }
         public int hour1 { get; set; }
         public int min10 { get; set; }
         public int min1 { get; set; }
         public int sec10 { get; set; }
         public int sec1 { get; set; }
-
     }
 }
