cab7530b56ca8ead67936f5580dfcfae562513e2
plisewski <32065830+plisewski@users.noreply.github.com>
2018-03-05 21:00:07 +0100

    Add files via upload

---
+++ BinaryClock/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace BinaryClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BinaryClock/ViewModel/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace BinaryClock.ViewModel
+{    
+    public class Timer : INotifyPropertyChanged
+    {
+        public DateTime CurrentTime
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged()
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(CurrentTime)));
+        }
+
+        public Timer()
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Tick += (sender, e) => { OnPropertyChanged(); };
+            dispatcherTimer.Start();
+        }
+    }
+}
